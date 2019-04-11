405de27f1733accb916dda02caae23c0c325152f
Eduardo Paixão <duduindo@gmail.com>
2019-04-10 23:38:15 -0300

    Jest added; Babel configured again

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+// import Vue from 'vue'
+// import App from './App.vue'
 
-new Vue({
-  el: '#app2',
-  render: h => h(App)
-})
+
+// new Vue({
+//   el: '#app2',
+//   render: h => h(App)
+// })
