7a2859dfca7fb46e549db82b472b0cf99c21adb9
Eduardo <duduindo@gmail.com>
2019-06-03 17:40:57 -0300

    Reduce code

--- src/content/IDBAdmin.ts
+++ src/content/IDBAdmin.ts
@@ -12,32 +12,34 @@ class IDBAdmin {
     return new Promise((resolve, reject) => {
       request.onsuccess = (event: any) => resolve(event)
       request.onerror = (event: any) => reject(event)
-      request.onblocked = (event: any) => reject(event)
+      request.onblocked = (event: any) => {
+        const env = {
+          ...event,
+          toString: () => `Altered version from ${event.oldVersion} to ${event.newVersion}`
+        }
+
+        reject(env)
+      }
     })
   }
 
   async getStoreNamesToArray(): Promise<IDBAdminResponse> {
     let db: Open
     let list: Array<string> = []
-    let text: string
 
     try {
       db = await this.open()
       list = Array.from(db.target.result.objectStoreNames)
-      text = 'Success'
 
       return {
         data: list,
-        text,
+        text: 'Success',
         type: 'success'
       }
     } catch(e) {
-      text = e.toString()
-      text = ('oldVersion' in e) ? `Altered version from ${e.oldVersion} to ${e.newVersion}` : text
-
       return {
         data: list,
-        text,
+        text: e.toString(),
         type: 'error'
       }
     }
