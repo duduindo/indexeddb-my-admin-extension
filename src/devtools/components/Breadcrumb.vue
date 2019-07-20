9115e5f9e4844467f141e87ba3d2b6e945c632f7
Eduardo Paixão <duduindo@gmail.com>
2019-07-20 13:57:31 -0300

    Breadcrumb added without style

--- src/devtools/components/Breadcrumb.vue
+++ src/devtools/components/Breadcrumb.vue
@@ -2,9 +2,9 @@
   <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
       <li class="breadcrumb-item"><router-link :to="'/'" >Server: {{ host }}</router-link></li>
-      <li class="breadcrumb-item" v-if="database"><a href="#">Database: TCGA_DB</a></li>
-      <li class="breadcrumb-item" v-if="store"><a href="#">Store: ACC_Clinical</a></li>
-      <li class="breadcrumb-item" v-if="index"><a href="#">Index: Status</a></li>
+      <li class="breadcrumb-item" v-if="database"><router-link :to="'/'" >Database: {{ database }}</router-link></li>
+      <li class="breadcrumb-item" v-if="store"><router-link :to="`/object-store/${database}/${version}/${store}/`" >Store: {{ store }}</router-link></li>
+      <li class="breadcrumb-item" v-if="index"><router-link :to="`/index/${database}/${version}/${store}/${index}/`" >Index: {{ index }}</router-link></li>
     </ol>
   </nav>
 </template>
@@ -28,22 +28,23 @@
       })
     },
     methods: {
-      ...mapActions({})
-    },
-    watch: {
-      '$route'(to) {
-        const {
-          database = '',
-          version = '',
-          store = '',
-          index = ''
-        } = to.params
+      ...mapActions({}),
+      setBreadcrumbFromParams(params = {}) {
+        const { database, version, store, index } = params
 
         this.database = database
         this.version = version
         this.store = store
         this.index = index
       }
+    },
+    watch: {
+      '$route'(to) {
+        this.setBreadcrumbFromParams(to.params)
+      }
+    },
+    created() {
+      this.setBreadcrumbFromParams(this.$route.params)
     }
   }
 </script>
