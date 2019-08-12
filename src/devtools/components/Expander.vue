4354007ea8bce9a093ecbd9697292c1368896f60
Eduardo Paixão <duduindo@gmail.com>
2019-08-12 00:24:31 -0300

    Creating Expander

--- src/devtools/components/Expander.vue
+++ src/devtools/components/Expander.vue
@@ -1,54 +1,45 @@
 <template>
-  <div>
-    <nav class="c-expander">
-      <h4>Extension</h4>
-      <ul>
-        <li>Settings</li>
-      </ul>
+  <nav class="c-expander">
+    <h4>Databases</h4>
+    <ul>
+      <li>
+        <span>New</span>
+      </li>
+      <li>
+        <button class="c-expander__toggle-action c-expander__toggle-action--active"><span></span></button>
+        <span>Database1</span>
+      </li>
+      <li>
+        <button class="c-expander__toggle-action c-expander__toggle-action--active"><span></span></button>
+        <span>Database2</span>
 
-      <h4>Databases</h4>
-      <ul>
-        <li>
-          <span>icon</span>
-          <span>New</span>
-        </li>
-        <li>
-          <button class="c-expander__toggle-action c-expander__toggle-action--active"><span></span></button>
-          <span>icon</span>
-          <span>Database1</span>
-        </li>
-        <li>
-          <button class="c-expander__toggle-action c-expander__toggle-action--active"><span></span></button>
-          <span>icon</span>
-          <span>Database2</span>
+        <ul>
+          <li>
+            <span>New</span>
+          </li>
+          <li>
+            <button class="c-expander__toggle-action"><span></span></button>
+            <span>Store 1</span>
 
-          <ul>
-            <li>
-              <span>icon</span>
-              <span>New</span>
-            </li>
-            <li>
-              <button class="c-expander__toggle-action"><span></span></button>
-              <span>icon</span>
-              <span>Store 1</span>
+            <ul>
+              <li>
+                <span>New</span>
+              </li>
 
-              <ul>
-                <li>
-                  <span>icon</span>
-                  <span>New</span>
-                </li>
+              <li>
+                <span>Index 1</span>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+    </ul>
 
-                <li>
-                  <span>icon</span>
-                  <span>Index 1</span>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </nav>
-  </div>
+    <h4>Extension</h4>
+    <ul>
+      <li>Settings</li>
+    </ul>
+  </nav>
 </template>
 
 <script>
