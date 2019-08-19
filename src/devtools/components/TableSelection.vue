d0031c70ed7b6d7e408e7be13e78a65c5462ccdd
Eduardo Paixão <eduardo.paixao@grupofolha.com.br>
2019-08-19 19:41:05 -0300

    Creating Table with selection

---
+++ src/devtools/components/TableSelection.vue
@@ -0,0 +1,46 @@
+<template>
+  <table class="pure-table">
+    <thead>
+      <tr>
+        <td></td>
+        <td :key="index" v-for="(html, index) in columns" v-html="html" />
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr :key="index" v-for="(line, index) in data">
+        <td><input type="checkbox"></td>
+        <td :key="indexValue" v-for="(value, indexValue) in line" v-html="value" />
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  export default {
+    name: 'table-selection',
+    props: {
+      columns: {
+        require: true,
+        default: () => ['#', 'Key (Key path: "<span>isbn</span>")', 'Value'],
+        type: Array
+      },
+      data: {
+        require: true,
+        default: () => [
+          [ 0, 'John', '{title: "Quarry Memories", author: "Fred", isbn: 123456}' ],
+          [ 1, 'Jane', '{title: "Quarry Memories", author: "Fred", isbn: 123456}' ],
+          [ 2, 'Susan', '{title: "Quarry Memories", author: "Fred", isbn: 123456}' ],
+          [ 3, 'Chris', '{title: "Quarry Memories", author: "Fred", isbn: 123456}' ],
+          [ 4, 'Dan', '{title: "Quarry Memories", author: "Fred", isbn: 123456}' ]
+        ],
+        type: Array
+      }
+    },
+    methods: {
+      handleClick() {
+        alert(10)
+      }
+    }
+  }
+</script>
--- src/devtools/pages/ObjectStore/components/TableResult.vue
+++ src/devtools/pages/ObjectStore/components/TableResult.vue
@@ -2,39 +2,22 @@
   <article>
     <form class="pure-form c-object-store-result" name="object-stores-list">
       <fieldset>
-        <table class="pure-table">
-          <thead>
-            <tr>
-              <td>#</td>
-              <td>Key (Key path: "<span>isbn</span>")</td>
-              <td>Value</td>
-            </tr>
-          </thead>
-
-          <tbody>
-            <tr :key="index" v-for="(field, index) in Array(20).fill('')" @click="handleSelected">
-              <td>{{index}}</td>
-              <td>{{Math.floor(Math.random() * 1e8)}}</td>
-              <td>{title: "Quarry Memories", author: "Fred", isbn: {{Math.floor(Math.random() * 1e8)}}}</td>
-            </tr>
-          </tbody>
-        </table>
+        <TableSelection />
       </fieldset>
     </form>
   </article>
 </template>
 
 <script>
+  import TableSelection from '@/devtools/components/TableSelection'
+
   export default {
     name: 'object-store-result',
-    methods: {
-      handleSelected(e) {
-        const tr = e.target.closest('tr')
-
-        // e.ctrlKey  = control
-        // e.shiftKey = shift
-
-        tr.classList.toggle('active')
+    components: { TableSelection },
+    data() {
+      return {
+        tableHead: [],
+        tableBody: []
       }
     }
   }
