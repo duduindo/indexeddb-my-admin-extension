82955dc25ffc5a3b3282a16e44d6ee0123db95a1
Eduardo Paixão <duduindo@gmail.com>
2019-09-03 18:59:22 -0300

    Adding textarea on  page Insert

--- src/devtools/pages/ObjectStore/children/Insert.vue
+++ src/devtools/pages/ObjectStore/children/Insert.vue
@@ -1,5 +1,19 @@
 <template>
-  <b v-html="label"></b>
+  <div class="container-fluid mt-3">
+    <div class="row" v-if="isPageClone">
+      <div class="col">
+        <table class="pure-table c-table c-table-theme-chrome">
+          <tbody>
+            <tr :key="index" v-for="(item, index) in [1,2,3,4,5,6,7,8,8,8,8,8,8]">
+              <td class="text-left">
+                <textarea name="" id="" cols="80" rows="10"></textarea>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,23 +21,16 @@
     name: 'object-store-insert',
     data() {
       return {
-        label: 'empty'
+        label: 'empty',
+        isPageAdd: false,
+        isPageClone: false
       }
     },
     mounted() {
       const path = this.$route.path
-      const isAdd = path.match(/\/insert\/add\/$/i)
-      const isClone = path.match(/\/insert\/clone\/$/i)
 
-      if (isAdd) {
-        this.label = 'Add'
-      }
-
-      if (isClone) {
-        this.label = 'Clone'
-      }
-
-      console.log(isAdd, isClone)
+      this.isPageAdd = path.match(/\/insert\/add\/$/i)
+      this.isPageClone = path.match(/\/insert\/clone\/$/i)
     }
   }
 </script>
