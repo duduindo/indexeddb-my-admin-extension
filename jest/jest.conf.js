a30b6cb261b336512a3a336e239abccf034f0496
Eduardo Paixão <duduindo@gmail.com>
2019-10-06 14:33:57 -0300

    Test configured

---
+++ jest/jest.conf.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = {
+  rootDir: path.resolve(__dirname, '../'),
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'vue',
+    'ts'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)'
+  ],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.vue$': 'vue-jest'
+  },
+  snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
+  setupFiles: [
+    '<rootDir>/jest/setup',
+    '<rootDir>/jest/jest.globals.js'
+  ],
+  coverageDirectory: '<rootDir>/jest/coverage',
+  collectCoverageFrom: [
+    'src/**/*.{js,ts,vue}',
+    '!src/content/app.js',
+    '!src/devtools/app.js',
+    '!src/devtools/router-config.js',
+    '!**/node_modules/**'
+  ]
+}
---
+++ jest/jest.globals.js
@@ -0,0 +1,6 @@
+// import IndexedDBFake from 'fake-indexeddb'
+// import IDBKeyRange from 'fake-indexeddb/lib/FDBKeyRange'
+
+
+// global.indexedDB = IndexedDBFake
+// global.IDBKeyRange = IDBKeyRange
