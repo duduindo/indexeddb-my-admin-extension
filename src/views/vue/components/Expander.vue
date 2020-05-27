a2f22129b63afd298316c7d457372e5aba8910dc
Eduardo Paixão <duduindo@gmail.com>
2020-05-27 18:23:44 -0300

    Added functions from Expanse

--- src/views/vue/components/Expander.vue
+++ src/views/vue/components/Expander.vue
@@ -1,11 +1,11 @@
 <template>
-  <el-menu :default-openeds="['1']">
+  <el-menu :default-openeds="['0']">
 
     <!-- Storage -->
     <el-submenu
       v-for="(storage, indexStorage) in storages"
       :key="indexStorage"
-      :index="String(indexStorage)"
+      :index="formatIndex(indexStorage)"
       >
       <template slot="title">{{ storage.name }}</template>
 
@@ -14,26 +14,38 @@
         <el-submenu
           v-for="(database, indexDatabase) in storage.databases"
           :key="indexDatabase"
-          :index="String(indexStorage) + '-' + String(indexDatabase)"
+          :index="formatIndex(indexStorage, indexDatabase)"
           >
-          <template slot="title"><div><i class="el-icon-coin"></i> {{ database.name }}</div></template>
+          <template slot="title">
+            <div @click="selectDatabase(storage.id, database.name, database.version)">
+              <i class="el-icon-coin"></i> {{ database.name }}
+            </div>
+          </template>
 
           <!-- Table -->
           <el-submenu
             v-for="(table, indexTable) in database.tables"
             :key="indexTable"
-            :index="String(indexStorage) + '-' + String(indexDatabase) + '-' + String(indexTable)"
+            :index="formatIndex(indexStorage, indexDatabase, indexTable)"
             >
-            <template slot="title"><div><i class="el-icon-coin"></i> {{ table.name }}</div></template>
+            <template slot="title">
+              <div @click="selectTable(storage.id, database.name, database.version, table.name)">
+                <i class="el-icon-coin"></i>
+                {{ table.name }}
+              </div>
+            </template>
 
             <!-- Index -->
             <el-menu-item-group>
               <el-menu-item
                 v-for="(index, i) in table.indexes"
                 :key="i"
-                :index="String(indexStorage) + '-' + String(indexDatabase) + '-' + String(indexTable)+ '-' + String(i)"
+                :index="formatIndex(indexStorage, indexDatabase, indexTable, i)"
               >
-                <div><i class="el-icon-notebook-2"></i> {{ index.name }}</div>
+                <div @click="selectIndex(storage.id, database.name, database.version, table.name, index.name)">
+                  <i class="el-icon-notebook-2"></i>
+                  {{ index.name }}
+                </div>
               </el-menu-item>
             </el-menu-item-group>
           </el-submenu>
@@ -47,6 +59,18 @@
 
 <script>
   export default {
+    methods: {
+      formatIndex: (...indexes) => indexes.join('-'),
+      selectDatabase(id, name, version) {
+        console.log(id, name, version)
+      },
+      selectTable(id, name, version, tablename) {
+        console.log(id, name, version, tablename)
+      },
+      selectIndex(id, name, version, tablename, indexname) {
+        console.log(id, name, version, tablename, indexname)
+      },
+    },
     computed: {
       storages() {
         return this.$store.getters.getExpanderStorages
