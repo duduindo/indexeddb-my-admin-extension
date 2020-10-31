58a053a1996485dffea698cf07d7e84a928fff92
Eduardo Paixão <duduindo@gmail.com>
2020-10-31 14:29:54 -0300

    Updated skeletons chrome

--- src/resources/skeletons/chrome/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-  // chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  //   if (changeInfo.status == 'complete') {
-  //     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-  //       chrome.tabs.sendMessage(tabs[0].id, {action: "SendIt sdsdsd"}, function(response) {});
-  //     });
-  //   }
-  // });
-})
