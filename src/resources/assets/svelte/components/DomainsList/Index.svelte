45601e2062aece85b8b3f46079961999aee87592
Eduardo Paixão <duduindo@gmail.com>
2021-01-02 20:36:57 -0300

    moved domain page to database/list

--- src/resources/assets/svelte/components/DomainsList/Index.svelte
+++ src/resources/assets/svelte/components/DomainsList/Index.svelte
@@ -4,7 +4,7 @@
 
 <div class="list-group">
   {#each $optionsTabs as option}
-    <a href="/pages/domain.html?origin={option.value}" class="list-group-item">
+    <a href="/pages/database/list.html?origin={option.value}" class="list-group-item">
       {option.text}
     </a>
   {/each}
