2ae2b09e92dfb3df6ab90421ee450542cffb2e32
Eduardo Paixão <duduindo@gmail.com>
2021-02-14 15:23:02 -0300

    Fonts moved; configuration build fonts

--- config/common.config.js
+++ config/common.config.js
@@ -4,6 +4,7 @@ const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const glob = require('glob');
 const webpack = require('webpack');
 const pages = require('./pages.json')
 
@@ -36,7 +37,12 @@ module.exports = (env, options) => {
           test: /\.s[ac]ss$/i,
           use: [
             MiniCssExtractPlugin.loader,
-            'css-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                url: false
+              }
+            },
             'sass-loader',
           ]
         },
@@ -126,19 +132,57 @@ module.exports = (env, options) => {
     // ### Entry
     entry: {
       // Content
-      content: resolve('src/resources/assets/content.ts'),
+      // content: resolve('src/resources/assets/content.ts'),
 
       // Background
-      background: resolve('src/resources/assets/background.ts'),
+      // background: resolve('src/resources/assets/background.ts'),
+
+      // Fonts
+  //     'fonts': glob.sync(resolve('src/resources/assets/fonts/*')).reduce((acc, path) => {
+  //   const entry = path.replace(/^.*[\\\/]/, '').replace('.js','');
+  //   acc[entry] = path;
+
+  //       console.log(acc[entry])
+
+  //   return acc;
+  // }, {}),
+
+    fonts: [
+  resolve('src/resources/assets/fonts/fa-brands-400.eot'),
+  resolve('src/resources/assets/fonts/fa-brands-400.svg'),
+  resolve('src/resources/assets/fonts/fa-brands-400.ttf'),
+  resolve('src/resources/assets/fonts/fa-brands-400.woff'),
+  resolve('src/resources/assets/fonts/fa-brands-400.woff2'),
+  resolve('src/resources/assets/fonts/fa-regular-400.eot'),
+  resolve('src/resources/assets/fonts/fa-regular-400.svg'),
+  resolve('src/resources/assets/fonts/fa-regular-400.ttf'),
+  resolve('src/resources/assets/fonts/fa-regular-400.woff'),
+  resolve('src/resources/assets/fonts/fa-regular-400.woff2'),
+  resolve('src/resources/assets/fonts/fa-solid-900.eot'),
+  resolve('src/resources/assets/fonts/fa-solid-900.svg'),
+  resolve('src/resources/assets/fonts/fa-solid-900.ttf'),
+  resolve('src/resources/assets/fonts/fa-solid-900.woff'),
+  resolve('src/resources/assets/fonts/fa-solid-900.woff2'),
+  resolve('src/resources/assets/fonts/icomoon.eot'),
+  resolve('src/resources/assets/fonts/icomoon.svg'),
+  resolve('src/resources/assets/fonts/icomoon.ttf'),
+  resolve('src/resources/assets/fonts/icomoon.woff'),
+  resolve('src/resources/assets/fonts/material-icons.svg'),
+  resolve('src/resources/assets/fonts/material-icons.ttf'),
+  resolve('src/resources/assets/fonts/material-icons.woff'),
+  resolve('src/resources/assets/fonts/summernote.eot'),
+  resolve('src/resources/assets/fonts/summernote.ttf'),
+  resolve('src/resources/assets/fonts/summernote.woff'),
+],
 
       // Pages
-      'pages/static/build': resolve('src/resources/assets/pages.ts'),
-      'pages/static/default': resolve('src/resources/assets/sass/default.sass'),
-      'pages/static/dark': resolve('src/resources/assets/sass/dark.sass'),
-      'pages/static/material': resolve('src/resources/assets/sass/material.sass'),
+      // 'pages/static/build': resolve('src/resources/assets/pages.ts'),
+      // 'pages/static/default': resolve('src/resources/assets/sass/default.sass'),
+      // 'pages/static/dark': resolve('src/resources/assets/sass/dark.sass'),
+      // 'pages/static/material': resolve('src/resources/assets/sass/material.sass'),
 
       // Popup
-      popup: resolve('src/resources/assets/popup.ts'),
+      // popup: resolve('src/resources/assets/popup.ts'),
     },
 
     // ### Output
