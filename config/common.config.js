dc4f202dc0b12cfe484ddaa793af52c5e8d5c297
Eduardo Paixão <duduindo@gmail.com>
2021-02-14 16:23:41 -0300

    Removido packages and config configured

--- config/common.config.js
+++ config/common.config.js
@@ -3,8 +3,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const glob = require('glob');
 const webpack = require('webpack');
 const pages = require('./pages.json')
 
@@ -93,7 +91,6 @@ module.exports = (env, options) => {
     // ### Plugins
     plugins: [
       new webpack.ProgressPlugin(),
-      new CleanWebpackPlugin(),
       new FixStyleOnlyEntriesPlugin(),
       new MiniCssExtractPlugin({
         filename: '[name].css',
@@ -124,7 +121,8 @@ module.exports = (env, options) => {
       // Copy
       new CopyPlugin({
         patterns: [
-          { from: resolve('src/resources/skeleton'), to: resolve('dist') }
+          { from: resolve('src/resources/skeleton'), to: resolve('dist') },
+          { from: resolve('src/resources/assets/fonts'), to: resolve('dist/fonts') },
         ]
       })
     ],
@@ -132,57 +130,19 @@ module.exports = (env, options) => {
     // ### Entry
     entry: {
       // Content
-      // content: resolve('src/resources/assets/content.ts'),
+      content: resolve('src/resources/assets/content.ts'),
 
       // Background
-      // background: resolve('src/resources/assets/background.ts'),
-
-      // Fonts
-  //     'fonts': glob.sync(resolve('src/resources/assets/fonts/*')).reduce((acc, path) => {
-  //   const entry = path.replace(/^.*[\\\/]/, '').replace('.js','');
-  //   acc[entry] = path;
-
-  //       console.log(acc[entry])
-
-  //   return acc;
-  // }, {}),
-
-    fonts: [
-  resolve('src/resources/assets/fonts/fa-brands-400.eot'),
-  resolve('src/resources/assets/fonts/fa-brands-400.svg'),
-  resolve('src/resources/assets/fonts/fa-brands-400.ttf'),
-  resolve('src/resources/assets/fonts/fa-brands-400.woff'),
-  resolve('src/resources/assets/fonts/fa-brands-400.woff2'),
-  resolve('src/resources/assets/fonts/fa-regular-400.eot'),
-  resolve('src/resources/assets/fonts/fa-regular-400.svg'),
-  resolve('src/resources/assets/fonts/fa-regular-400.ttf'),
-  resolve('src/resources/assets/fonts/fa-regular-400.woff'),
-  resolve('src/resources/assets/fonts/fa-regular-400.woff2'),
-  resolve('src/resources/assets/fonts/fa-solid-900.eot'),
-  resolve('src/resources/assets/fonts/fa-solid-900.svg'),
-  resolve('src/resources/assets/fonts/fa-solid-900.ttf'),
-  resolve('src/resources/assets/fonts/fa-solid-900.woff'),
-  resolve('src/resources/assets/fonts/fa-solid-900.woff2'),
-  resolve('src/resources/assets/fonts/icomoon.eot'),
-  resolve('src/resources/assets/fonts/icomoon.svg'),
-  resolve('src/resources/assets/fonts/icomoon.ttf'),
-  resolve('src/resources/assets/fonts/icomoon.woff'),
-  resolve('src/resources/assets/fonts/material-icons.svg'),
-  resolve('src/resources/assets/fonts/material-icons.ttf'),
-  resolve('src/resources/assets/fonts/material-icons.woff'),
-  resolve('src/resources/assets/fonts/summernote.eot'),
-  resolve('src/resources/assets/fonts/summernote.ttf'),
-  resolve('src/resources/assets/fonts/summernote.woff'),
-],
+      background: resolve('src/resources/assets/background.ts'),
 
       // Pages
-      // 'pages/static/build': resolve('src/resources/assets/pages.ts'),
-      // 'pages/static/default': resolve('src/resources/assets/sass/default.sass'),
-      // 'pages/static/dark': resolve('src/resources/assets/sass/dark.sass'),
-      // 'pages/static/material': resolve('src/resources/assets/sass/material.sass'),
+      'pages/static/build': resolve('src/resources/assets/pages.ts'),
+      'pages/static/default': resolve('src/resources/assets/sass/default.sass'),
+      'pages/static/dark': resolve('src/resources/assets/sass/dark.sass'),
+      'pages/static/material': resolve('src/resources/assets/sass/material.sass'),
 
       // Popup
-      // popup: resolve('src/resources/assets/popup.ts'),
+      popup: resolve('src/resources/assets/popup.ts'),
     },
 
     // ### Output
