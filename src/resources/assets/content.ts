3435c144b3aa8b0b24c097b1568d5d5fc41b4666
Eduardo Paixão <duduindo@gmail.com>
2021-02-18 22:10:28 -0300

    Adding limitless plugins

--- src/resources/assets/content.ts
+++ src/resources/assets/content.ts
@@ -1,78 +1,76 @@
-// import Router from 'routes'
-// import browser from 'webextension-polyfill'
-// import type { RouterAction } from '@/models/FetchContent/types'
-// import type { DatabaseStruture, IDBDatabaseInfo } from '@/models/Database/types'
+import Router from 'routes'
+import browser from 'webextension-polyfill'
+import type { RouterAction } from '@/models/FetchContent/types'
+import type { DatabaseStruture, IDBDatabaseInfo } from '@/models/Database/types'
 
-// // Database
-// import IndexedDB from '@/models/Database/IndexedDB'
+// Database
+import IndexedDB from '@/models/Database/IndexedDB'
 
-// const router = Router();
-
-
-// // Database
-// // =========================================================
-// router.addRoute('https?\://:domain/:name/:version/delete/', function() {
-//   const { name, version } = this.params;
-
-//   return IndexedDB.deleteDatabase(name)
-// });
-
-// router.addRoute('https?\://:domain/databases/', function() {
-//   return IndexedDB.getDatabases()
-// });
-
-// router.addRoute('https?\://:domain/:name/:version/structure/', function() {
-//   const { name, version } = this.params;
-//   const connection = IndexedDB.openDatabase(name, version)
-//   const indexedDB = new IndexedDB(connection)
-
-//   return indexedDB.getStructureFromDatabase()
-// });
-
-// router.addRoute('https?\://:domain/structure-databases/', async function(): Promise<DatabaseStruture[]> {
-//   const databases: IDBDatabaseInfo[] = await IndexedDB.getDatabases()
-//   let structures = []
-
-//   structures = databases.map(({ name, version }) => {
-//     const connection = IndexedDB.openDatabase(name, version)
-//     const indexedDB = new IndexedDB(connection)
-
-//     return indexedDB.getStructureFromDatabase()
-//   })
-
-//   return await Promise.all(structures)
-// });
+const router = Router();
 
 
-// // Table
-// // =========================================================
-// router.addRoute('https?\://:domain/:name/:version/table-names/', function() {
-//   const { name, version } = this.params;
-//   const connection = IndexedDB.openDatabase(name, version)
-//   const indexedDB = new IndexedDB(connection)
+// Database
+// =========================================================
+router.addRoute('https?\://:domain/:name/:version/delete/', function() {
+  const { name, version } = this.params;
 
-//   return indexedDB.getTableNames()
-// })
+  return IndexedDB.deleteDatabase(name)
+});
 
-// router.addRoute('https?\://:domain/:name/:version/:table/table-content/', function() {
-//   const { name, version, table } = this.params;
-//   const connection = IndexedDB.openDatabase(name, version)
-//   const indexedDB = new IndexedDB(connection)
+router.addRoute('https?\://:domain/databases/', function() {
+  return IndexedDB.getDatabases()
+});
 
-//   return indexedDB.getContentFromTable(table)
-// })
+router.addRoute('https?\://:domain/:name/:version/structure/', function() {
+  const { name, version } = this.params;
+  const connection = IndexedDB.openDatabase(name, version)
+  const indexedDB = new IndexedDB(connection)
+
+  return indexedDB.getStructureFromDatabase()
+});
+
+router.addRoute('https?\://:domain/structure-databases/', async function(): Promise<DatabaseStruture[]> {
+  const databases: IDBDatabaseInfo[] = await IndexedDB.getDatabases()
+  let structures = []
+
+  structures = databases.map(({ name, version }) => {
+    const connection = IndexedDB.openDatabase(name, version)
+    const indexedDB = new IndexedDB(connection)
+
+    return indexedDB.getStructureFromDatabase()
+  })
+
+  return await Promise.all(structures)
+});
 
 
-// // Listener
-// // =========================================================
-// browser.runtime.onMessage.addListener((action: RouterAction) => {
-//   const match = router.match(action.url)
+// Table
+// =========================================================
+router.addRoute('https?\://:domain/:name/:version/table-names/', function() {
+  const { name, version } = this.params;
+  const connection = IndexedDB.openDatabase(name, version)
+  const indexedDB = new IndexedDB(connection)
 
-//   if (match) {
-//     return match.fn(action.data)
-//   }
+  return indexedDB.getTableNames()
+})
 
-//   return Promise.reject(new Error('Route not found'))
-// })
+router.addRoute('https?\://:domain/:name/:version/:table/table-content/', function() {
+  const { name, version, table } = this.params;
+  const connection = IndexedDB.openDatabase(name, version)
+  const indexedDB = new IndexedDB(connection)
+
+  return indexedDB.getContentFromTable(table)
+})
 
 
+// Listener
+// =========================================================
+browser.runtime.onMessage.addListener((action: RouterAction) => {
+  const match = router.match(action.url)
+
+  if (match) {
+    return match.fn(action.data)
+  }
+
+  return Promise.reject(new Error('Route not found'))
+})
--- src/resources/assets/pages.ts
+++ src/resources/assets/pages.ts
@@ -4,8 +4,8 @@
 
 // // Components
 // import Breadcrumb from './svelte/components/Breadcrumb/Index.svelte'
-// import StorageTree from './svelte/components/StorageTree/Index.svelte'
-// import DomainsList from './svelte/components/DomainsList/Index.svelte'
+import StorageTree from './svelte/components/StorageTree/Index.svelte'
+import DomainsList from './svelte/components/DomainsList/Index.svelte'
 // import DatabaseCreate from './svelte/components/DatabaseCreate/Index.svelte'
 // import DatabaseImport from './svelte/components/DatabaseCreate/Import.svelte'
 // import DatabasesList from './svelte/components/DatabasesList/Index.svelte'
@@ -14,10 +14,20 @@
 
 
 // document.querySelectorAll('[data-breadcrumb-main]').forEach(target => new Breadcrumb({ target }))
-// document.querySelectorAll('[data-storage-tree]').forEach(target => new StorageTree({ target }))
-// document.querySelectorAll('[data-domains-list]').forEach(target => new DomainsList({ target }))
+document.querySelectorAll('[data-storage-tree]').forEach(target => new StorageTree({ target }))
+document.querySelectorAll('[data-domains-list]').forEach(target => new DomainsList({ target }))
 // document.querySelectorAll('[data-database-create]').forEach(target => new DatabaseCreate({ target }))
 // document.querySelectorAll('[data-database-import]').forEach(target => new DatabaseImport({ target }))
 // document.querySelectorAll('[data-databases-list]').forEach(target => new DatabasesList({ target }))
 // document.querySelectorAll('[data-tables-list]').forEach(target => new TablesList({ target }))
 // document.querySelectorAll('[data-table-browser]').forEach(target => new TableBrowser({ target }))
+
+// import $ from 'jquery'
+// import { createTree } from 'jquery.fancytree'
+
+
+
+// createTree('.tree-json', {
+//  // extensions: ['edit', 'filter'],
+
+// });
